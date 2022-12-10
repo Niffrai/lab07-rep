8ef6aacf78e191a8dd4ff7132864907a94f701a4
Niffrai <mrniffrai3115@gmail.com>
2022-12-10 17:54:56 +0300

    sql poradok idet tak a-d > aa-dd > aaa-ccc

---
+++ lab7-b.sql
@@ -0,0 +1,1 @@
+CREATE TABLESPACE space22 OWNER "user7" LOCATION '/media/dobrinya/e47b3702-1585-4d34-8bbe-2e718c015a4c/space2';
---
+++ lab7-c.sql
@@ -0,0 +1,3 @@
+CREATE TABLESPACE space4 OWNER "user7" LOCATION '/home/dobrinya/Documents/space4';
+
+
